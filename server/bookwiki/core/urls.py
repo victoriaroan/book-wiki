27b5d7c850cb471791b1c2f7bee32eefbfcec9f9
victoria <victoriaroan@gmail.com>
2017-08-31 17:58:52 -0400

    user stuff.

--- server/bookwiki/core/urls.py
+++ server/bookwiki/core/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^login/$', views.CoreLoginView.as_view(), name='login'),
     url(r'^logout/$', views.CoreLogoutView.as_view(), name='logout'),
-    url(r'^profile/(?P<username>\w+)/$', login_required(views.ProfileView.as_view()), name='profile'),
-    url(r'^profile/(?P<username>\w+)/edit/$', login_required(views.ProfileEditView.as_view()), name='profile'),
+    url(r'^profile/(?P<username>\w+)/$', login_required(views.ProfileView.as_view()), name='profile-view'),
+    url(r'^profile/(?P<username>\w+)/edit/$', login_required(views.ProfileEditView.as_view()), name='profile-edit'),
 ]
--- server/bookwiki/core/views.py
+++ server/bookwiki/core/views.py
@@ -16,6 +16,7 @@ class CoreLogoutView (LayoutMixin, LogoutView):
     pass
 
 class ProfileView (LayoutMixin, DetailView):
+    layout_template = LayoutTemplates.LEFT_ASIDE
     model = get_user_model()
     slug_field = 'username'
     slug_url_kwarg = 'username'
@@ -27,8 +28,8 @@ class ProfileEditView (PermissionRequiredMixin, LayoutMixin, UpdateView):
     slug_url_kwarg = 'username'
     permission_required = 'users.can_edit'
     form_class = ProfileForm
-    
+
     def has_permission(self):
         can_edit = PermissionRequiredMixin.has_permission(self)
         return can_edit or self.request.user.pk == self.get_object()
-    
+
